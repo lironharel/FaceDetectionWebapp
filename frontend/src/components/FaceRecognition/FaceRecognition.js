f01673243368d2e7baa9e15ee00e0546468679fd
Liron Harel <lironharel@gmail.com>
2022-11-13 15:07:11 +0200

    css changes

--- frontend/src/components/FaceRecognition/FaceRecognition.js
+++ frontend/src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = (props) => {
     const { imgUrl, box } = props;
     return (
         <div className="center ma">
-            <div className="absolute mt2">
+            <div className="absolute mt4">
                 {imgUrl.length > 0
                 ? <img id="face-image" src={imgUrl} width="500px" height="auto" alt="face" /> 
                 : ""}
--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -6,14 +6,14 @@ const Navigation = ({setRoute, isSignedIn}) => {
   if(isSignedIn) {
     NavItems =
     <>
-      <p onClick={() => setRoute('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+      <p onClick={() => setRoute('signin')} className='f4 link dim black underline pa3 pointer'>Sign Out</p>
     </> 
   } 
   else {
     NavItems =  
     <>
-      <p onClick={() => setRoute('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-      <p onClick={() => setRoute('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+      <p onClick={() => setRoute('signin')} className='f4 link dim black underline pa3 pointer'>Sign In</p>
+      <p onClick={() => setRoute('register')} className='f4 link dim black underline pa3 pointer'>Register</p>
     </>
   }
   
